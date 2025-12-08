02d1423d0e59e1186b5eb95bb7e8bd2646d9a53f
ottotse <otto1996w@gmail.com>
2025-12-08 15:52:53 +0800

    var

---
+++ Goodbye.java
@@ -0,0 +1,29 @@
+public class Goodbye {
+  public static void main(String[] args) {
+    System.out.println("Goodbye, World!");
+    System.out.println("你好，\n世界！");
+
+    //int > bye, short, byte, long
+    //float > double
+    //Single Character > char A/B/C
+    //true or false > boolean
+
+    int x = 10;
+    int y = 7;
+    System.out.println(x + y);
+
+    x = 20;
+    System.out.println(x + y);
+
+    y = 100;
+    System.out.println(x + y);
+
+    //double
+    double a = 0.52222;
+    double b = 0.1123123;
+    System.out.println(a + b);
+
+  }
+    
+  
+}
